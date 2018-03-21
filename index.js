e59240e6854f3af0a406e0fff552ae3a5d01bfb7
Adrian Estrada <edsadr@gmail.com>
2018-03-21 09:11:02 -0400

    Mutators (Functions to insert and update Messages)

--- index.js
+++ index.js
@@ -4,26 +4,15 @@ const { buildSchema } = require('graphql')
 const fs = require('fs')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const root = require('./lib/resolvers')
 const path = require('path')
 
 const app = express()
 const port = process.env.port || 3000
 const schemaCode = fs.readFileSync(path.join(__dirname, 'lib', 'schema.graphql'), 'utf8')
-const storage = require('./lib/storage')
 
 // Defining a initial schema with GraphQL schema language
 const schema = buildSchema(schemaCode)
-
-// Setup a resolver for the query defined in the schema
-const root = {
-  getMessage: (args) => {
-    const message = storage.filter((item) => item.id === args.id)
-    return message[0]
-  },
-  getAllMessages: (args) => {
-    return storage
-  }
-}
 
 // Serve the GraphQL in /api using an express middleware
 app.use('/api', gqlMiddleware({
